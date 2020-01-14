07b64520b4de5d04e34bd522904b66de9a3f84e8
Elerimlmao <59827643+Elerimlmao@users.noreply.github.com>
2020-01-14 11:55:12 +0100

    Create funkcijenaloga3.py

---
+++ funkcijenaloga3.py
@@ -0,0 +1,8 @@
+def lihochecker():
+    x = int(input("Vnesi število: "))
+    if (x%2) == 0:
+        print("To je sodo število.")
+    if (x%2) > 0:
+        print("To je liho število.")
+
+lihochecker()
